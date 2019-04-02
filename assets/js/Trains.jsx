89e050a2b9a8dd7a7204b6c5d24bd0fa673390da
Avery Peterson <peterson.a@husky.neu.edu>
2019-04-02 12:29:34 -0400

    Making trains collapsible

--- assets/js/Trains.jsx
+++ assets/js/Trains.jsx
@@ -3,13 +3,21 @@ import { connect } from 'react-redux';
 
 function Trains(props) {
   let { predictions } = props;
-  let stops = _.map(predictions, (p_arr) => {
-    return <Stop nexts={p_arr} />;
+  let i = 0;
+  let stops = _.map(predictions, p_arr => {
+    i++;
+    return <Stop key={i} nexts={p_arr} />;
   });
-  return <div className="trains-wrapper">
-            <h2>Next Trains</h2>
-            { stops }
-        </div>
+  return (
+    <div className="trains-wrapper">
+      <a href="#trains" data-toggle="collapse">
+        Next Trains
+      </a>
+      <div id="trains" className="collapse">
+        <table>{stops}</table>
+      </div>
+    </div>
+  );
 }
 
 function Stop(props) {
@@ -19,12 +27,25 @@ function Stop(props) {
   let dir = nexts[0].dir;
   let dest = nexts[0].dest;
   let color = nexts[0].color;
-  let arr1 = nexts[0].status ? nexts[0].status : parseInt(nexts[0].min_to_arrival);
-  let arr2 = nexts[1].status ? nexts[1].status : parseInt(nexts[1].min_to_arrival);
+  let arr1 = nexts[0].status
+    ? nexts[0].status
+    : parseInt(nexts[0].min_to_arrival) +
+      (parseInt(nexts[0].min_to_arrival) === 1 ? ' min' : ' mins');
+  let arr2 = nexts[1].status
+    ? nexts[1].status
+    : parseInt(nexts[1].min_to_arrival) +
+      (parseInt(nexts[1].min_to_arrival) === 1 ? ' min' : ' mins');
 
-  return <div>
-          <div><span style={{color: `#${color}`}}>{route}:</span> {stop} -> {dest} {arr1}, {arr2} mins</div>
-        </div>;
+  return (
+    <tr>
+      <td style={{ color: `#${color}` }}>{route}</td>{' '}
+      <td>
+        {stop} -> {dest}
+      </td>
+      <td>{arr1}</td>
+      <td>{arr2}</td>
+    </tr>
+  );
 }
 
 function state2props(state) {
